9c60ded1a92ed071dff79065a1460a242f900059
amirreza besmi <amirreza.besmi.work@gmail.com>
2023-01-18 10:16:01 +0330

    #BPW-15 feat(Food): add food get all query with its handler implementation

---
+++ RestaurantDDD.Application/Foods/Queries/GetAll/GetAllFoodsQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using RestaurantDDD.Domain.Entities;
+
+namespace RestaurantDDD.Application.Foods.Queries.GetAll;
+
+public class GetAllFoodsQuery : IRequest<IEnumerable<Food>>
+{
+}
---
+++ RestaurantDDD.Application/Foods/Queries/GetAll/GetAllFoodsQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RestaurantDDD.Domain.Entities;
+using RestaurantDDD.Infrastructure.Data;
+
+namespace RestaurantDDD.Application.Foods.Queries.GetAll;
+
+public class GetAllFoodsQueryHandler : IRequestHandler<GetAllFoodsQuery, IEnumerable<Food>>
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public GetAllFoodsQueryHandler(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<IEnumerable<Food>> Handle(GetAllFoodsQuery request, CancellationToken cancellationToken)
+    {
+        var foods = await _dbContext.Food.ToListAsync();
+        return foods;
+    }
+}
